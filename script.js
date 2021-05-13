d69bac846ce4f515333b035b0d284b5c4e31f818
Michael Abreha <michaelabreha@Michaels-MacBook-Pro.local>
2021-05-13 00:23:58 -0400

    Made many changes to the html and css file, created contact page and a footer. Project not completed yet still have to fix some issues.

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-        if(this.scrollY > 20){
-            $('.navbar').addClass("sticky");
-}else{
-    $('.navbar').removeClass("sticky");
-}
-    })
-});
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if (this.scrollY > 20) {
+      $(".navbar").addClass("sticky");
+    } else {
+      $(".navbar").removeClass("sticky");
+    }
+  });
 
-//toggle menu/navbar script
+  //toggle menu/navbar script
 
-$('.menu-btn').click(funtion(){
-
-    $('.navbar .menu').toggleClass('active');
-
+  $(".menu-btn").click(function () {
+    $(".navbar .menu").toggleClass("active");
+    $(".menu-btn i").toggleClass("active");
+  });
 });
